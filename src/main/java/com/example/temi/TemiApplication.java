9efe80217f46b2de71e0f5a76f1a2409e265a4c4
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-09-24 18:50:07 +0900

    :tada: temi
    시작

---
+++ src/main/java/com/example/temi/TemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.temi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemiApplication.class, args);
+	}
+
+}
