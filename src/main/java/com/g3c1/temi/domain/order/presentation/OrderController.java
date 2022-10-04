d17a0360a04b92150859ae39861aff71f9528cb1
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-10-04 20:08:44 +0900

    :sparkles: 패키지 생성
    ㅇㅇ

---
+++ src/main/java/com/g3c1/temi/domain/order/presentation/OrderController.java
@@ -0,0 +1,11 @@
+package com.g3c1.temi.domain.order.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("order")
+public class OrderController {
+}
---
+++ src/main/java/com/g3c1/temi/domain/seat/presentation/SeatController.java
@@ -0,0 +1,11 @@
+package com.g3c1.temi.domain.seat.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("seat")
+public class SeatController {
+}
