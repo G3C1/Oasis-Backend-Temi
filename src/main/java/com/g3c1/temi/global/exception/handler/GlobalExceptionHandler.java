36f6f7ab15e12a95d407e5176a8bbddeb0f48180
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-10-19 15:58:55 +0900

    :sparkles: 에러핸들러
    ㅇㅇ

--- src/main/java/com/g3c1/temi/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/g3c1/temi/global/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.g3c1.temi.global.exception.handler;
 
+import com.g3c1.temi.domain.purchase.exception.FoodNotFoundException;
+import com.g3c1.temi.domain.seat.exception.SeatNotDisableException;
+import com.g3c1.temi.domain.seat.exception.SeatNotEnableException;
 import com.g3c1.temi.domain.seat.exception.SeatNotFoundException;
 import com.g3c1.temi.global.exception.ErrorResponse;
 import org.springframework.core.Ordered;
@@ -14,6 +17,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(SeatNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleSeatNotFoundException(SeatNotFoundException exception){
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode().getMessage(), exception.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(exception.getErrorCode().getStatus()));
+    }
+    @ExceptionHandler(SeatNotDisableException.class)
+    public ResponseEntity<ErrorResponse> handleSeatNotDisableException(SeatNotDisableException exception){
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode().getMessage(), exception.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(exception.getErrorCode().getStatus()));
+    }
+    @ExceptionHandler(FoodNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleFoodNotFoundException(FoodNotFoundException exception){
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode().getMessage(), exception.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(exception.getErrorCode().getStatus()));
+    }
+    @ExceptionHandler(SeatNotEnableException.class)
+    public ResponseEntity<ErrorResponse> handleSeatNotEnableException(SeatNotEnableException exception){
         ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode().getMessage(), exception.getErrorCode().getStatus());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(exception.getErrorCode().getStatus()));
     }
